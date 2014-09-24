9aaa813b483027bba5fe0d9a6825e407f7c3c012
LBainWx <lamont.bain@gmail.com>
2014-09-23 22:07:42 -0500

    Update Storm_Data.py

--- Storm_Data.py
+++ Storm_Data.py
@@ -3,6 +3,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 import pylab as pl
 import matplotlib.image as mpimg
+import sys
 
 f = open("C:\Users\Lamont\Desktop\Misc_WFO_FWD\FWD_torsV2.csv", 'r')
 all_tor = f.readlines()
@@ -29,35 +30,72 @@ start_lon            = []
 end_lat              = []
 end_lon              = []
 county               = []
-county_index         = []
+
 #cumulative stats
 year_tor_count       = []
 year_count           = []
 
+#indexing to sort
+county_index         = []
+event_index          = []
+
 for i in range(1, len(all_tor)):
 	wrk1.append(str(all_tor[i]).split(','))
 
-for i in range(0, len(wrk1)-1):
-	year.append(int(wrk1[i][0]))
-	month.append(int(wrk1[i][1]))
-	day.append(int(wrk1[i][2]))
-	date.append((wrk1[i][3]). translate(None, '/'))
-	time.append((wrk1[i][4]).translate(None, ':'))
-	rate.append(int(wrk1[i][9]))
-	county.append(str(wrk1[i][27]).translate(None, '\n'))	
-	county_index.append((wrk1[i][27]).translate(None, '\n'))
+for i in range(0, len(wrk1)):
+	year.append(int(wrk1[i][1]))
+	month.append(int(wrk1[i][2]))
+	day.append(int(wrk1[i][3]))
+	date.append((wrk1[i][4]).translate(None, '/'))
+	event_index.append(str(wrk1[i][4])) #.translate(None, '/'))
+	time.append((wrk1[i][5]).translate(None, ':'))
+	rate.append(int(wrk1[i][10]))
+	county.append(str(wrk1[i][28]).translate(None, '\n'))	
+	county_index.append((wrk1[i][28]).translate(None, '\n'))
+
+co_list = sorted(set(county))
+
+#for i in range(0, len(co_list)):
+	#print co_list[i]
+#
+#print county
+
+
+
+#--------------------Date Specific Statistics---------------------------------#
+#Sort by specific date (e.g., for Outbreaks)
+
+#Event date needs to be in mmddyyyy format
+#Don't use leading zero for months/days with single digits (e.g. use 5 instead of 05 for May)
+
+event_date = '4/26/2011'
+event_index = [k for k, event_index in enumerate(event_index) if event_index == event_date]
+print event_index
+#for i in range(0, len(event_index)):
+#    print county[event_index[i]], rate[event_index[i]], time[event_index[i]]
+
+#print len(event_index)
+sys.exit() 
 
 
 
 #---------------------County Based Statistics----------------------------------#	
 #Return the Index for a given County for various statistics
+
 #The County for stats to be computed. 
-co_name = 'Lamar'
+co_name = 'Dallas'
 county_index = [k for k, county_index in enumerate(county_index) if county_index == co_name]
 
-#Print Total Number of TORs by County.
-print 'The tornado count for',co_name,'County is:', county.count(co_name)
 
+#Print Total Number of TORs by County.
+	#print 'The tornado count for',co_list[i],'County is:', county.count(co_list[i])
+	#print county.count(co_list[i])
+	#continue
+
+
+sys.exit()
+
+TOR_count = str(county.count(co_name))
 #Need individual arrays for ratings in a given county
 EF0_Co = []
 EF1_Co = []
@@ -67,21 +105,23 @@ EF4_Co = []
 EF5_Co = []
 
 #Sort the number of tornadoes by rating for a given county
-#for i in range(0, len(county_index)):
-    #Print entire list of whatever tornado stat wanted by county
+for i in range(0, len(county_index)):
+#Print entire list of whatever tornado stat wanted by county
 #	print county[county_index[i]], "EF",(rate[county_index[i]])	
-#	if rate[county_index[i]] == 0:
-#		EF0_Co.append(rate.count(0))
-#	elif rate[county_index[i]] == 1:
-#		EF1_Co.append(rate.count(1))
-#	elif rate[county_index[i]] == 2:
-#		EF2_Co.append(rate.count(2))
-#	elif rate[county_index[i]] == 3:
-#		EF3_Co.append(rate.count(3))
-#	elif rate[county_index[i]] == 4:
-#		EF4_Co.append(rate.count(4))	
-#	elif rate[county_index[i]] == 5:
-#		EF5_Co.append(rate.count(5))	
+	if rate[county_index[i]] == 0:
+		EF0_Co.append(rate.count(0))
+	elif rate[county_index[i]] == 1:
+		EF1_Co.append(rate.count(1))
+	elif rate[county_index[i]] == 2:
+		EF2_Co.append(rate.count(2))
+	elif rate[county_index[i]] == 3:
+		EF3_Co.append(rate.count(3))
+	elif rate[county_index[i]] == 4:
+		EF4_Co.append(rate.count(4))	
+	elif rate[county_index[i]] == 5:
+		EF5_Co.append(rate.count(5))	
+	
+
 	
 #Needed to do this number of if/elif statements...may be an easier way to do this
 #in the future, but right now this appears to work really well. 
@@ -102,7 +142,7 @@ EF5_Co = []
 #print "The total number of violent (F/EF-4 or F/EF-5) tornadoes for", co_name, "County :", np.add(len(EF4_Co), len(EF5_Co))
 
 #Print out total number of F/EF-5 Tornadoes
-#print "The total number of F/EF-5 tornadoes for", co_name, "County :", (len(EF4_Co))	
+#print "The total number of F/EF-5 tornadoes for", co_name, "County :", (len(EF5_Co))	
 	
 
 #Sort the total number of tornadoes by month for a given county	
@@ -113,11 +153,14 @@ total_month = []
 for i in range(0, len(county_index)):
 	for j in range(len(mon_co_name)):
 		if month[county_index[i]]     == j+1:
-			month[county_index[i]]    =  mon_co_name[j]
+				month[county_index[i]]    =  mon_co_name[j]
 			
 for i in range(len(mon_co_name)):
 	total_month.append(int(month.count(mon_co_name[i])))
 print total_month, sum(total_month)
+
+#Terminate program here
+sys.exit()
 
 climo_dur = (max(year)-min(year)+1)
 climo_yr_co = np.arange(climo_dur)+min(year)
@@ -135,9 +178,12 @@ for j in range(min(year), max(year)+1):
 	#yrly_co_county.append(int(j))
 	
 
+max_year = str(max(year))
+min_year = str(min(year))
 
+#print min_year, "-", max_year
+	
 #print year_tor_count_co, sum(year_tor_count_co)
-
 #Plot # of TORs in a County By Year
 #print len(year_tor_count_co), len(climo_yr_co)
 #print climo_yr_co
@@ -153,7 +199,7 @@ for j in range(min(year), max(year)+1):
 #Plot # of TORS in a County By Month
 pl.xlim(0.5,12.5)
 mon_num = [1,2,3,4,5,6,7,8,9,10,11,12]
-img = ("NWS_log.png")
+#img = ("NWS_log.png")
 #print len(mon_num)
 
 
@@ -161,7 +207,7 @@ plt.xticks(mon_num, mon_co_name)
 plt.bar(mon_num, total_month, align='center', label = "Tornadoes", color='red')
 pl.ylim(0,max(total_month)+5)
 #plt.grid()
-plt.suptitle('Number of Tornadoes by Month between 1950-2011 in '+co_name+' County \n NOAA\NWS Fort Worth, Texas')
+plt.suptitle('Number of Tornadoes by Month between '+min_year+'-'+max_year+' in '+co_name+' County is: '+TOR_count+' \n NOAA\NWS Fort Worth, Texas')
 plt.xlabel('Months')
 plt.ylabel('Number of Tornadoes')
 plt.legend(('Tornado',), loc='upper right')
@@ -175,11 +221,20 @@ for i in range(len(mon_num)):
 #plt.show()	
 #plt.text(12, 20, 'EXPERIMENTAL', fontsize=50, color='gray', ha='right', va='bottom',
 #alpha=0.5)
-plt.savefig('C:\Users\Lamont\Desktop\Misc_WFO_FWD\Plots\Month\ '+co_name+'.png')
-			
+#plt.savefig('C:\Users\Lamont\Desktop\Misc_WFO_FWD\Plots\Month\ '+co_name+'.png')
+
+#a = np.array(total_month)
+#print a.transpose()
+#myfile = file("Lamar_test.txt", "w")
+#myfile = file("Lamar_test.txt", "a")
+#f.write(total_month)			
+#print >>myfile, total_month
+#myfile.close()
+
 #print max(year)
 #print min(year)
 #print total_mon_co
+
 
 #Sort the total number of tornadoes by month for a given county	
 
@@ -187,6 +242,7 @@ plt.savefig('C:\Users\Lamont\Desktop\Misc_WFO_FWD\Plots\Month\ '+co_name+'.png')
 
 #-----------------------CWA Wide Statistics--------------------------------------------# 
 
+#county_index = [k for k, county_index in enumerate(county_index) if county_index == co_list]
 #Print the Total Number of TORs by Year for the entire CWA. 
 #for j in range(min(year), max(year)+1):
 #	year_tor_count.append(year.count(j))
